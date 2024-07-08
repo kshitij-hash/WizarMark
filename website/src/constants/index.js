65889e6439fd2feeadfc0d14401e5723d030c2c0
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-08 23:03:15 +0530

    constants index

---
+++ website/src/constants/index.js
@@ -0,0 +1,16 @@
+import { input, save, sidepanel } from "../assets/index";
+
+export const images = [
+  {
+    id: 0,
+    src: input,
+  },
+  {
+    id: 1,
+    src: save,
+  },
+  {
+    id: 2,
+    src: sidepanel,
+  },
+];
