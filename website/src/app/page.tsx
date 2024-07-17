09aef226ca079658d571983e4bea37db4ff41f00
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-17 23:59:47 +0530

    features section

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,19 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import WaitlistForm from "@/components/waitlist-form";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 import { images } from "../constants/index";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -12,7 +25,39 @@ import {
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen w-full overflow-hidden">
+    <div
+      className="flex flex-col min-h-screen w-full overflow-hidden"
+      suppressHydrationWarning
+    >
+      <header className="flex justify-between px-5 py-2">
+        <Link href="/" className="text-2xl">
+          WizarMark
+        </Link>
+        <div className="flex gap-2">
+          <Dialog>
+            <DialogTrigger>
+              <Button>Join Waitlist</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <WaitlistForm />
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+          <a
+            href="https://github.com/kshitij-hash/WizarMark"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button variant="ghost">
+              <GitHubLogoIcon />
+            </Button>
+          </a>
+          <ModeToggle />
+        </div>
+      </header>
+      <Separator />
+
       <main className="flex-grow">
         <section className="px-5 pt-10 flex flex-col items-center">
           <h1 className="text-4xl font-bold mb-4">Welcome to WizarMark</h1>
@@ -57,6 +102,21 @@ export default function Home() {
             <CarouselPrevious />
             <CarouselNext />
           </Carousel>
+        </section>
+
+        {/* Features */}
+        <section>
+          <div className="w-full min-h-screen mt-20 mx-auto">
+              <h1 className="text-4xl font-extrabold tracking-widest text-center">Features</h1>
+              <div className="flex items-center justify-center">
+                <ul className="font-semibold text-3xl leading-[70px] flex text-center flex-col justify-center items-start mt-10">
+                  <li>Save Bookmark on the go</li>
+                  <li>Navigate easily through side panel</li>
+                  <li>Search your saved Bookmark by tags or keywords</li>
+                  <li>Fetch all your bookmarks at one place</li>
+                </ul>
+              </div>
+          </div>
         </section>
       </main>
       <Separator />
