03171546ca5e8baeaa351f31077dec631f2fb04b
KSHITIJ <83551355+kshitij-hash@users.noreply.github.com>
2024-07-18 21:02:47 +0530

    Revert "features section"

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-// export { default } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
 
 export async function middleware(request: NextRequest) {
     const url = request.nextUrl;
